388cc801a8a817df1567b504f351ad3994ceee47
glaucia86 <glaucia_lemos86@hotmail.com>
2019-04-07 23:07:47 -0300

    Desenvolvimento do Front-End

---
+++ lab-2/workshop-2/front-end/js/core.js
@@ -0,0 +1,8 @@
+/**
+ * Arquivo: js/core.js
+ * Descrição: Arquivo responsável pela execução principal do Angular
+ * Data: 07/04/2019
+ * Author: Glaucia Lemos
+ */
+
+angular.module('todoList', ['todoController', 'todoService']);
